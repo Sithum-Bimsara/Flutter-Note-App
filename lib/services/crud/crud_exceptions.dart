c5451a47063a0754e1bfca0357d9c3bcd7bdcc68
sithum-code-s <sithumbimsara59@gmail.com>
2026-02-09 14:01:37 +0530

    feat: crud operations implemented

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,15 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectory implements Exception {}
+
+class DatabaseIsNotOpenException implements Exception {}
+
+class CouldNotDeleteUserException implements Exception {}
+
+class UserAlreadyExistsException implements Exception {}
+
+class CouldNotFindUserException implements Exception {}
+
+class CouldNotDeleteNoteException implements Exception {}
+
+class CouldNotUpdateNoteException implements Exception {}
